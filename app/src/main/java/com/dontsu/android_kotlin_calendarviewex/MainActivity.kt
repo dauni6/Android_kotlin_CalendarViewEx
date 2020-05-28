cb5527f7d2a9b4ab73c12b0bb325713845a3f1e7
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-28 22:14:52 +0900

    func
    func

--- app/src/main/java/com/dontsu/android_kotlin_calendarviewex/MainActivity.kt
+++ app/src/main/java/com/dontsu/android_kotlin_calendarviewex/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dontsu.android_kotlin_calendarviewex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         setMinDate()
 
         getToday()
+
+        setMaxDate()
     }
 
     private fun getToday() {
@@ -36,5 +39,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun setMinDate() {
         calendarView.minDate = Date().time //오늘날짜포함 이후로 선택가능, 최소날짜
+    }
+
+    private fun setMaxDate() {
+        //현재 년, 월, 일
+        val year = Calendar.getInstance().get(Calendar.YEAR)
+        val month = Calendar.getInstance().get(Calendar.MONTH)
+        val day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+
+        Log.d("결괏값", " $year 년 ${month + 1} 월 $day 일")
+
+        manipulateDate(year, month, day)
+
+    }
+
+    private fun manipulateDate(year: Int, month: Int, day: Int) {
+        //최대 60일간 빌릴 수 있음 maxDay는 +60일 한 날짜로 만들어준다.
+        
     }
 }
