36443a4aff174f0c708eefd5aaf09e6287567a06
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-28 22:56:57 +0900

    d
    ddd

--- app/src/main/java/com/dontsu/android_kotlin_calendarviewex/MainActivity.kt
+++ app/src/main/java/com/dontsu/android_kotlin_calendarviewex/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,6 +56,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun manipulateDate(year: Int, month: Int, day: Int) {
         //최대 60일간 빌릴 수 있음 maxDay는 +60일 한 날짜로 만들어준다.
-        
+        var count = 60
+        var flag = true
+        val mdays = ArrayList<ArrayList<Int>>()
+        val normalYear = arrayListOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31) //평년
+        val leapYear = arrayListOf(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31) //윤년
+        mdays.add(normalYear)
+        mdays.add(leapYear)
+
+        while (flag) {
+            
+        }
+    }
+
+    private fun isLeap(year: Int): Int { //윤년 : 1 , 평년 : 0
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return 1
+        }
+        return 0
     }
 }
